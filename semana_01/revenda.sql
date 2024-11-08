68b2eaeda302d56762871e634d6590b6c0ab03df
JOSE BRENO OLIVEIRA DA SILVA <jose_bo_silva@estudante.sc.senai.br>
2024-11-07 21:51:41 -0300

    07-11-24

--- semana_01/revenda.sql
+++ semana_01/revenda.sql
@@ -81,3 +81,55 @@ select from veiculo order by preco_venda asc limit 3 offset 3
 
 -- apaga tabela
 drop table veiculo
+
+--------------------------------------------------------------------------
+alter table manutencao {
+	add column veiculo_id NOT NULL 
+}
+ALTER TABLE vendas
+ADD COLUMN metodo_pagamento metodo_pagamento;
+
+delete from vendedor where id = 10;
+
+SELECT * FROM vendas
+SELECT * FROM manutencao
+SELECT * FROM cliente
+
+CREATE TYPE metodo_pagamento AS ENUM ('cartao', 'pix', 'boleto');
+
+INSERT INTO cliente (nome, cpf, telefone, email, endereco) VALUES
+('adrian', '000.000.000-01', '48 0000-0000', 'adrian@gmail.com', 'mata-atlantica'),
+('alan', '111.000.000-02', '48 1111-1111', 'alan@gmail.com', 'bosque da mata'),
+('geovanni', '000.111.000-03', '48 2222-2222', 'geovani@gmail.com', 'centro'),
+('luiz', '000.000.111-04', '48 3333-3333', 'luizinho24@gmail.com', 'flora-flora'),
+('rosane', '222.000.000-05', '48 4444-4444', 'roro@gmail.com', 'mata-floresta')
+
+select * FROM vendedor
+
+INSERT INTO vendedor (nome, cpf, telefone, email, data_contratacao) VALUES
+('rosanilda', '22200011110', '48 56565656', 'roronildinha@gmail.com', '15/10/2015'),
+('alane', '11122200007', '48 23232323', 'alanilda@gmail.com', '03/03/2021'),
+('luiza insonsa', '22200011109', '48 45454545', 'luizinhoviadinho24@gmail.com', '25/11/2003'),
+('geovannela', '20011122208', '48 34343434', 'geovanellafodona@gmail.com', '03/04/2021'),
+('adriano', '10000011106', '48 12121212', 'adrianinfodao@gmail.com', '02/03/2020')
+
+SELECT AVG(quilometragem) FROM veiculo
+SELECT AVG(quilometragem) AS MEDIA_QUILOMETRAGEM FROM veiculo
+--ROUND arredonda o numero de casas conforme solicitado
+SELECT ROUND(AVG(quilometragem),2) FROM veiculo
+SELECT SUM(preco_venda) FROM veiculo
+--SUM soma os precos --WHERE filtrar conforme solicitado
+SELECT ROUND(SUM(preco_venda),1) FROM veiculo WHERE cor = 'branco'
+
+--BETWEEN filtra, e tras os resultados entre os espicificados
+SELECT SUM(valor_total) FROM vendas WHERE data_venda BETWEEN '2024/11/15' AND '2025/11/15'
+
+SELECT  * FROM vendas ORDER BY valor_total --Ordena
+SELECT  * FROM vendas ORDER BY valor_total ASC --Ordena em forma crescente
+SELECT  * FROM vendas ORDER BY valor_total DESC -- Ordena de forma decrescente
+
+--LIKE Filtra pelo que voce pede 
+SELECT * FROM cliente WHERE nome LIKE '%nho%'
+
+SELECT * FROM cliente ORDER BY nome DESC
+
